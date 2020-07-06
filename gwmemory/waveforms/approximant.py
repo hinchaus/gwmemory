b1f95131cd7120ad0a5a5e5b82694297cce90312
Colm Talbot <colm.talbot@ligo.org>
2020-07-06 11:49:19 -0700

    add higher-mode approximant with lalsim.ChooseTDModes

--- gwmemory/waveforms/approximant.py
+++ gwmemory/waveforms/approximant.py
@@ -158,3 +158,148 @@ class Approximant(MemoryGenerator):
             return h_lm, times
         else:
             return combine_modes(h_lm, inc, phase), times
+
+
+class HigherModeApproximant(MemoryGenerator):
+
+    def __init__(self, name, q, total_mass=60, spin_1=None, spin_2=None,
+                 distance=400, times=None, lmax=4, delta_t=None, phase=0,
+                 minimum_frequency=20, reference_frequency=20):
+        """
+        Initialise Surrogate MemoryGenerator
+
+        Parameters
+        ----------
+        name: str
+            File name to load.
+        q: float
+            Binary mass ratio
+        total_mass: float, optional
+            Total binary mass in solar units.
+        distance: float, optional
+            Distance to the binary in MPC.
+        spin_1: array-like
+            Spin vector of more massive black hole.
+        spin_2: array-like
+            Spin vector of less massive black hole.
+        times: array-like
+            Time array to evaluate the waveforms on, default is time array
+            from lalsimulation.
+            FIXME
+        """
+        self.name = name
+        if q > 1:
+            q = 1 / q
+
+        self.q = q
+        self.MTot = total_mass
+        if spin_1 is None:
+            self.S1 = np.array([0., 0., 0.])
+        else:
+            self.S1 = np.array(spin_1)
+        if spin_2 is None:
+            self.S2 = np.array([0., 0., 0.])
+        else:
+            self.S2 = np.array(spin_2)
+        self.distance = distance
+
+        self.m1 = self.MTot / (1 + self.q)
+        self.m2 = self.m1 * self.q
+        self.m1_SI = self.m1 * SOLAR_MASS
+        self.m2_SI = self.m2 * SOLAR_MASS
+        self.distance_SI = self.distance * MPC
+
+        self.S1 = list(self.S1)
+        self.S2 = list(self.S2)
+        self.lmax = lmax
+        self.available_modes = list({(2, 2), (2, -2)})
+
+        self.h_to_geo = (
+            self.distance_SI / (self.m1_SI+self.m2_SI) / GG * CC ** 2
+        )
+        self.t_to_geo = 1 / (self.m1_SI+self.m2_SI) / GG * CC ** 3
+
+        self.h_lm = None
+        self.times = None
+        self.minimum_frequency = minimum_frequency
+        self.reference_frequency = reference_frequency
+
+        h_lm, times = self.time_domain_oscillatory(delta_t=delta_t, phase=phase)
+
+        MemoryGenerator.__init__(self, name=name, h_lm=h_lm, times=times)
+
+    def time_domain_oscillatory(self, delta_t=None, modes=None, inc=None,
+                                phase=None):
+        """
+        Get the mode decomposition of the waveform approximant.
+
+        Since the waveforms we consider only contain content about the
+        ell=|m|=2 modes.
+        We can therefore evaluate the waveform for a face-on system, where
+        only the (2, 2) mode is non-zero.
+
+        Parameters
+        ----------
+        delta_t: float, optional
+            Time step for waveform.
+        modes: list, optional
+            List of modes to try to generate.
+        inc: float, optional
+            Inclination of the source, if None, the spherical harmonic modes
+            will be returned.
+        phase: float, optional
+            Phase at coalescence of the source, if None, the spherical harmonic
+            modes will be returned.
+
+        Returns
+        -------
+        h_lm: dict
+            Spin-weighted spherical harmonic decomposed waveform.
+        times: np.array
+            Times on which waveform is evaluated.
+        """
+        if self.h_lm is None:
+
+            if phase is None:
+                phase = 0.0
+            approx = lalsim.GetApproximantFromString(self.name)
+            WFdict = None
+
+            if delta_t is None:
+                delta_t = 0.1 * (self.m1_SI + self.m2_SI) * GG / CC ** 3
+            else:
+                delta_t = delta_t
+
+            waveform = lalsim.SimInspiralChooseTDModes(
+                phase, delta_t, self.m1_SI, self.m2_SI,
+                self.S1[0], self.S1[1], self.S1[2],
+                self.S2[0], self.S2[1], self.S2[2],
+                self.minimum_frequency, self.reference_frequency,
+                self.distance_SI, WFdict, self.lmax, approx
+            )
+
+            h_lm = dict()
+            mode = (waveform.l, waveform.m)
+            if modes is None or mode in modes:
+                h_lm[mode] = waveform.mode.data.data
+            while waveform.next is not None:
+                waveform = waveform.next
+                mode = (waveform.l, waveform.m)
+                if modes is not None and mode not in modes:
+                    continue
+                h_lm[mode] = waveform.mode.data.data
+
+            times = np.linspace(
+                0, delta_t * len(waveform.mode.data.data),
+                len(waveform.mode.data.data)
+            )
+            times -= times[np.argmax(abs(h_lm[(2, 2)]))]
+
+        else:
+            h_lm = self.h_lm
+            times = self.times
+
+        if inc is None or phase is None:
+            return h_lm, times
+        else:
+            return combine_modes(h_lm, inc, phase), times
